f1359dde4fddfee1b25f8bc5978dcf9f7f2f9a64
Rheina <rheina.tamara@outlook.com>
2020-11-05 21:53:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function divide(){
 function add(a,b){
   return a+b;
 }
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
